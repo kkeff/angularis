32332403c16acd463cf7c31b58e26d5e8403f394
Kasper Gustavii <kasper.gustavii@live.se>
2017-06-14 18:45:08 +0200

    Changed build configuration

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -9,7 +9,7 @@ import { HeroService } from './hero.service';
   template: `
               <h1>{{title}}</h1>
               <hero-detail [hero]="selectedHero"></hero-detail>
-              <h2>My Heroes</h2>
+              <h2>My Heroess</h2>
               <ul class="heroes">
                 <li *ngFor="let hero of heroes"
                     (click)="onSelect(hero)"
